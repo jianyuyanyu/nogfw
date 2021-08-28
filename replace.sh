7138bc10ead644728fe44bdc987c7a81c09f2116
gfw-breaker <lixiaoming0513@gmail.com>
2021-08-28 11:06:44 +0800

    ok

--- replace.sh
+++ replace.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-org=$1
-new=$2
+#org=$1
+#new=$2
+
+#sed -i "s#$org#$new#g" README.md
+
+
+org="https://cdn.jsdelivr.net/gh/easy2view/nogfw"
+new="http://45.63.99.126:10000/videos/sw/binary"
 
 sed -i "s#$org#$new#g" README.md
 
